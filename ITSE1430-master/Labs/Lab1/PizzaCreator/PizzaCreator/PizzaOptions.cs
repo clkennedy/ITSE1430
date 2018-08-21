62fb77be7a7e646dd18225752f4e34ef3c5c85db
cameron.kennedy <cameron.kennedy@my.tccd.edu>
2018-08-20 19:22:34 -0500

    more Stroy 4 setup

--- ITSE1430-master/Labs/Lab1/PizzaCreator/PizzaCreator/PizzaOptions.cs
+++ ITSE1430-master/Labs/Lab1/PizzaCreator/PizzaCreator/PizzaOptions.cs
@@ -8,6 +8,16 @@ namespace PizzaCreator
 {
     class PizzaOption
     {
+        public static List<PizzaSize> AllSizes = new List<PizzaSize>(){
+                                                        new PizzaSize("Small", 5),
+                                                        new PizzaSize("Medium", 6.25),
+                                                        new PizzaSize("Large", 8.75)};
+
+        public static List<Meat> AllMeats = new List<Meat>(){ new Meat("Bacon", .75),
+                                                        new Meat("Ham", .75),
+                                                        new Meat("Papperoni", .75),
+                                                        new Meat("Sausage", .75)};
+
 
         protected string _name;
         protected double _cost;
---
+++ ITSE1430-master/Labs/Lab1/PizzaCreator/PizzaCreator/Vegetable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCreator
+{
+    class Vegetable : PizzaOption
+    {
+        public Vegetable(string name, double cost) : base(name, cost)
+        {
+
+        }
+    }
+}
